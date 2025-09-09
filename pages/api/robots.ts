556d1089bf4adc71744be6c062145c5dd542b3af
YuriPBT <yuri.pbt@outlook.com>
2025-09-09 11:45:38 +0200

    Update robots.ts

--- pages/api/robots.ts
+++ pages/api/robots.ts
@@ -3,3 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader("Content-Type", "text/plain");
   res.write(`User-agent: *
+Allow: /
+Sitemap: ${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`);
+  res.end();
+}
